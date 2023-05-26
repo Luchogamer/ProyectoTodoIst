82eb9f02515d9dfcd4ca42e39284c253b1eb840b
Luis Carlos Rios Zamora <85584471+Luchogamer@users.noreply.github.com>
2023-05-26 10:46:35 -0400

    Add files via upload

--- src/code/page/LeftSection.cs
+++ src/code/page/LeftSection.cs
@@ -11,6 +11,8 @@ namespace ProyectoTodoist.src.code.page
 	public class LeftSection
 	{
 		public Button newProject = new Button(By.XPath("//button[@aria-label='Añadir proyecto']"));
+		public Button theProyect = new Button(By.XPath("//a[contains(.,('nuevo proyecto'))]"));
+	
 		public Button modifyProject = new Button(By.XPath("//div[@class='eYDM03d0TdWUmvQvarxM6w== DebMdpIqKcFRpFpVm5-wrA== _2a3b75a1 _509a57b4 e5a9206f _50ba6b6b']/../div//button[@type='button']"));
 		public Button deleteProject = new Button(By.XPath("//div[contains(text(),'Eliminar proyecto')]"));
 		public Button confirmDelete = new Button(By.XPath("//button[@data-autofocus='true']"));
--- src/code/page/ProjectMenuCreation.cs
+++ src/code/page/ProjectMenuCreation.cs
@@ -8,6 +8,9 @@ namespace ProyectoTodoist.src.code.page
 	{
         public TextBox nameProject = new TextBox(By.Id("edit_project_modal_field_name"));
         public Button createProject = new Button(By.XPath("//button[@type='submit']"));
+        public Button creations = new Button(By.XPath("//button[@class='plus_add_button']"));
+        public TextBox newTask = new TextBox(By.XPath("//p[@data-placeholder='Nombre de la tarea']"));
+        public Button acceptTask = new Button(By.XPath("//button[@data-testid='task-editor-submit-button']"));
     }
 }
 
